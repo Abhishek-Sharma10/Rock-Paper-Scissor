2854907677d25aa6a9e480be67d704d5940b3157
Abhishek-Sharma10 <nandanprasadjagat@gmail.com>
2024-10-30 15:51:56 +0530

    add

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let userScore = 0;
 let compScore = 0;
 let drawScore = 0;
@@ -62,68 +61,3 @@ choices.forEach((choice)=>{
         playGame(choiceId);
     })
 })
-=======
-let userScore = 0;
-let compScore = 0;
-let drawScore = 0;
-
-let choices = document.querySelectorAll(".button");
-let msg = document.querySelector(".msg");
-
-let userScorePara = document.querySelector("#count-you");
-let compScorePara = document.querySelector("#count-comp");
-let drawScorePara = document.querySelector("#count-draw");
-
-let genCompChoice = () =>{
-    let options = ["rock","paper","scissors"];
-    let randIdx = Math.floor(Math.random()*3);
-    return options[randIdx];
-}
-
-let showWinner = (userWin,userChoice,compChoice) => {
-    if(userWin){
-        userScore++;
-        userScorePara.innerText = userScore;
-        msg.innerText = `You Win! ${userChoice} beats ${compChoice}`;
-        msg.style.backgroundColor = "green";
-    } else {
-        compScore++;
-        compScorePara.innerText = compScore;
-        msg.innerText = `You lose! ${compChoice} beats ${userChoice}`;
-        msg.style.backgroundColor = "Red";
-    }
-}
-
-let drawGame = () => {
-    drawScore++;
-    drawScorePara.innerText = drawScore;
-    msg.innerText = "Game was draw. Play again";
-    msg.style.backgroundColor = "rgb(0, 166, 172)";
-}
-
-let playGame = (userChoice) =>{
-    let compChoice = genCompChoice();
-
-    if(userChoice === compChoice){
-        drawGame();
-    } else {
-        let userWin = true;
-        if(userChoice === "rock"){
-            userWin = (compChoice === "paper") ? false : true;
-        } else if(userChoice === "paper"){
-            userWin = (compChoice === "scissors") ? false : true;
-        } else {
-            userWin = (compChoice === "rock") ? false : true;
-        }
-        showWinner(userWin,userChoice,compChoice);
-    }
-};
-
-
-choices.forEach((choice)=>{
-    choice.addEventListener("click",()=>{
-        let choiceId = choice.getAttribute("id");
-        playGame(choiceId);
-    })
-})
->>>>>>> 96e35291d8dc738c5c08a61cf80332dcf23a8374
